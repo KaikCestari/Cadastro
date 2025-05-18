837786c4db6e17410af4ec1a373f89c3dd164f4c
KaikCestari <kaikcestaridev@gmail.com>
2025-05-18 03:15:55 -0300

    criei uma entidade e tabela

---
+++ src/main/java/dev/kaik7x/Cadastro/Usuariocontroller.java
@@ -0,0 +1,13 @@
+package dev.kaik7x.Cadastro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class Usuariocontroller {
+    @GetMapping ("/boasvindas")
+    public String boasVindas(){
+  return 0;
+    }
+
+}
--- src/main/java/dev/kaik7x/Cadastro/controller.java
+++
@@ -1,4 +0,0 @@
-package dev.kaik7x.Cadastro;
-
-public class controller {
-}
