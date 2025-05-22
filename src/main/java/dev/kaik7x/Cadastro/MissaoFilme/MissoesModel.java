2b92a14c755c0178524bc4fee043bed96ae3f1d4
KaikCestari <kaikcestaridev@gmail.com>
2025-05-22 15:56:13 -0300

    FFF

--- src/main/java/dev/kaik7x/Cadastro/MissaoFilme/MissoesModel.java
+++ src/main/java/dev/kaik7x/Cadastro/MissaoFilme/MissoesModel.java
@@ -1,7 +1,8 @@
 package dev.kaik7x.Cadastro.MissaoFilme;
-
 import dev.kaik7x.Cadastro.Usuarios.UsuarioModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
@@ -12,6 +13,15 @@ public class MissoesModel {
     private String nomeMissao;
     private String dificuldade;
     private UsuarioModel usuarios;
+
+    @OneToMany(mappedBy = "missoes")
+    private List<UsuarioModel> ninjas;
+
+
+
+
+
+
     //Construtores
     public MissoesModel() {
     }
@@ -23,6 +33,35 @@ public class MissoesModel {
         this.usuarios = usuarios;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getNomeMissao() {
+        return nomeMissao;
+    }
+
+    public void setNomeMissao(String nomeMissao) {
+        this.nomeMissao = nomeMissao;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public UsuarioModel getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(UsuarioModel usuarios) {
+        this.usuarios = usuarios;
+    }
 }
--- src/main/java/dev/kaik7x/Cadastro/Usuarios/UsuarioModel.java
+++ src/main/java/dev/kaik7x/Cadastro/Usuarios/UsuarioModel.java
@@ -14,44 +14,17 @@ public class UsuarioModel{
     private String nome;
     private String email;
     private int idade;
-    private int senha;
+    // UM Ninja tem uma unica missao
+    @ManyToOne
+    @JoinColumn(name = "Missoes_id")
+    private  MissoesModel missoes;
 
     public UsuarioModel() {
-    }
-
-    public UsuarioModel(int senha) {
-        this.senha = senha;
-    }
-
-    public int getSenha() {
-        return senha;
-    }
-
-    public void setSenha(int senha) {
-        this.senha = senha;
     }
 
     public UsuarioModel(String nome, int idade, String email) {
         this.nome = nome;
         this.idade = idade;
         this.email = email;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public int getIdade() {
-        return idade;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
     }
 }
